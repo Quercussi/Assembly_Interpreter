5cada4271d929203ae09dbc8cfcc114650f2aead
FrienDDDD <friendjunk@gmail.com>
2023-09-26 17:47:19 +0700

    fixed error

---
+++ src/RInstruction.java
@@ -0,0 +1,30 @@
+public class RInstruction {
+   
+    private static RInstruction instance ;
+
+    private RInstruction(){} ;
+
+    public static RInstruction getInstance(){
+        if(instance == null){
+            instance = new RInstruction();
+        }
+
+        return instance ;
+    }
+
+    public void executeR(Stage stage,int opcode,int rs,int rt,int rd){
+        
+        int []reg = stage.getRegister() ;
+
+        if(rd == 0){
+            return ;
+        }
+
+        if(opcode == 0){        // add instruction               
+            reg[rd] = reg[rs] + reg[rt] ;
+        }else if(opcode == 1){  // nand instruction
+            reg[rd] = ~(reg[rs] & reg[rt]) ;
+        }
+    }
+
+}
